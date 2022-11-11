c5cbab9a1aefe08fe8289d6fd6bad6c45c4251b1
purveshsakhiya9 <purvesh.sakhiya@somaiya.edu>
2022-11-11 13:56:59 -0500

    updated connection .py

--- connection.py
+++ connection.py
@@ -1,24 +1,26 @@
 import mysql.connector as mysql
 
-
-mydb = mysql.connect(
-  host="localhost",
-  user="root",
-  password="root"
-)
-mycursor = mydb.cursor()
+host="localhost"
+user="root"
+password="root"
 
 try:
-  mycursor.execute("CREATE DATABASE Car")
-  print("Database Created Successfully")
+  mydb = mysql.connect(host=host,username=user,password=password)
+  print("connected to mysql server.")
+  try:
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE Car")
+    print("Database Created Successfully")
+  except Exception as e:
+    print("Could not connect with database...database already exists")
+    print("connecting with database")
+    mydb1 = mysql.connect(
+      host="localhost",
+      user="root",
+      password="root",
+      database="Car"
+    )
 except Exception as e:
-  print("Could not connect with database...database already exists")
-  print("connecting with database")
-  mydb = mysql.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="Car"
-  )
+  print(e)
 
 
